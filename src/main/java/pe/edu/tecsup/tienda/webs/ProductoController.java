b1ffd8f79043cfb75b78a0e67486a943a0b08fd4
jgomezz <jgomezz@gmail.com>
2024-12-01 12:59:13 -0500

    update

--- src/main/java/pe/edu/tecsup/tienda/webs/ProductoController.java
+++ src/main/java/pe/edu/tecsup/tienda/webs/ProductoController.java
@@ -1,21 +1,32 @@
 package pe.edu.tecsup.tienda.webs;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import pe.edu.tecsup.tienda.entities.Categoria;
 import pe.edu.tecsup.tienda.entities.Producto;
 import pe.edu.tecsup.tienda.services.CategoriaService;
 import pe.edu.tecsup.tienda.services.ProductoService;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.Date;
 import java.util.List;
 
 @Slf4j
 @Controller
 @RequestMapping("/productos")
 public class ProductoController {
+
+    @Value("${app.storage.path}")
+    private String STORAGEPATH;
+
 
     CategoriaService categoriaService;
     ProductoService productoService;
@@ -48,5 +59,31 @@ public class ProductoController {
 
         return "productos/create";
     }
+
+    @PostMapping("/store")
+    public String store(@ModelAttribute("producto") Producto producto, Errors errors,
+                        @RequestParam("file") MultipartFile file,
+                        RedirectAttributes redirectAttrs) throws Exception{
+        log.info("call store(producto: " + producto + ")");
+
+        if(file != null && !file.isEmpty()) {
+            String filename = System.currentTimeMillis() + file.getOriginalFilename().substring(file.getOriginalFilename().lastIndexOf("."));
+            producto.setImagen_nombre(filename);
+            if(Files.notExists(Paths.get(STORAGEPATH))){
+                Files.createDirectories(Paths.get(STORAGEPATH));
+            }
+            Files.copy(file.getInputStream(), Paths.get(STORAGEPATH).resolve(filename), StandardCopyOption.REPLACE_EXISTING);
+        }
+
+        producto.setCreado(new Date());
+        producto.setEstado(1);
+
+        productoService.save(producto);
+
+        redirectAttrs.addFlashAttribute("message", "Registro guardado correctamente");
+
+        return "redirect:/productos";
+    }
+
 
 }
