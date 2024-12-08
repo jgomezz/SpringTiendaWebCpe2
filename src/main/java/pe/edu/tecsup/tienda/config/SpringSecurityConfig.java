2dd7d37d6ffc9a6dee3fd391aaa298d5331f357a
jgomezz <jgomezz@gmail.com>
2024-12-08 11:39:25 -0500

    update

--- src/main/java/pe/edu/tecsup/tienda/config/SpringSecurityConfig.java
+++ src/main/java/pe/edu/tecsup/tienda/config/SpringSecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -11,6 +12,8 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -66,7 +69,35 @@ public class SpringSecurityConfig {
     }
     */
 
+    // Autorization
 
+    //*
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+            // Configure authorizations
+            .authorizeHttpRequests((requests) -> requests
+                            .requestMatchers("/" , "/webjars/**", "/css/**","/error/**").permitAll()
+                            .requestMatchers("/productos").authenticated()
+//             .requestMatchers("/admin/**").hasAnyAuthority("Administrador")
+                )
+                // Change login
+                .formLogin((form) -> form
+                        .loginProcessingUrl("/authenticate")
+                        .loginPage("/login").permitAll()
+                )
+                //*
+                .logout((logout) -> logout
+                        .logoutUrl("/logout")
+                        .logoutSuccessUrl("/login")
+                )
+                // Change csrf
+                .csrf( (csrf) -> csrf.disable());
+        //*/
+        return http.build();
+    }
+    //*/
 
 
 }
