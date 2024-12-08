6bab392ba2f41a4141dafff781cf0d1cb6b0ef80
jgomezz <jgomezz@gmail.com>
2024-12-08 08:32:40 -0500

    update

--- src/main/java/pe/edu/tecsup/tienda/webs/ProductoController.java
+++ src/main/java/pe/edu/tecsup/tienda/webs/ProductoController.java
@@ -85,5 +85,43 @@ public class ProductoController {
         return "redirect:/productos";
     }
 
+    @GetMapping("/edit/{id}")
+    public String edit(@PathVariable Long id, Model model) throws Exception {
+        log.info("edit edit(id: " + id + ")");
+
+        List<Categoria> categorias = categoriaService.findAll();
+        model.addAttribute("categorias", categorias);
+
+        Producto producto = productoService.findById(id);
+        model.addAttribute("producto", producto);
+
+        return "productos/edit";
+    }
+
+    @PostMapping("/update")
+    public String update(@ModelAttribute("producto") Producto producto, Errors errors,
+                         @RequestParam("file") MultipartFile file,
+                         RedirectAttributes redirectAttrs) throws Exception{
+        log.info("call update(producto: " + producto + ")");
+
+        if(file != null && !file.isEmpty()) {
+            String filename = System.currentTimeMillis() + file.getOriginalFilename().substring(file.getOriginalFilename().lastIndexOf("."));
+            producto.setImagen_nombre(filename);
+            if(Files.notExists(Paths.get(STORAGEPATH))){
+                Files.createDirectories(Paths.get(STORAGEPATH));
+            }
+            Files.copy(file.getInputStream(), Paths.get(STORAGEPATH).resolve(filename), StandardCopyOption.REPLACE_EXISTING);
+        }
+
+        productoService.save(producto);
+
+        redirectAttrs.addFlashAttribute("message", "Registro guardado correctamente");
+
+        return "redirect:/productos";
+    }
+
+
+
+
 
 }
